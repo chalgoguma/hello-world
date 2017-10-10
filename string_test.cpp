c22234a5434275a6c91223cb371cd9dbbbe4fe83
jaekwang <chalgoguma@gmail.com>
2017-10-10 16:37:42 +0900

    fdfds

--- string_test.cpp
+++ string_test.cpp
@@ -44,18 +44,6 @@ bool isRightsideGE(const std::string& left, const std::string& right)
 
 int main()
 {
-	std::string ver1 = "1.0.0";
-	
-	isRightsideGE(ver1, ver2);
-
-	// stringstream test...
-	int intNum = 0;
-	double doubleNum = 0;
-
-	std::stringstream stream1;
-	//std::string testString1 = "25 1 3 .235\n1111\n222222";	
-	std::string testString1 = "25,1.3 .235<76>\n1111\n222222";
-
 	stream1.str(testString1);
 
 	while (stream1 >> intNum)
