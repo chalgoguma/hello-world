094aa9b8a77b8cc0dc25f2efafad4458fdcf526d
chalgoguma <chalgoguma@gmail.com>
2017-10-10 16:40:55 +0900

    Test new branch (#3)
    * add c++ file
    
    * deleted a line
    
    * fdfds
    
    * 23232

---
+++ string_test.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+
+#include <iostream>  
+#include <sstream>
+
+bool isRightsideGE(const std::string& left, const std::string& right)
+{
+	std::stringstream lvsstr(left);
+	std::stringstream rvsstr(right);
+
+	int leftVer = 0, rightVer = 0;
+	
+	bool existVer = true;
+	while (existVer)
+	{
+		int leftVer = 0, rightVer = 0;
+		existVer = false;
+
+		if (lvsstr >> leftVer)
+		{
+			if (lvsstr.peek() == '.')
+				lvsstr.ignore();
+
+			existVer = true;
+		}
+
+		if (rvsstr >> rightVer)
+		{
+			if (rvsstr.peek() == '.')
+				rvsstr.ignore();
+
+			existVer = true;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	stream1.str(testString1);
+
+	while (stream1 >> intNum)
+	{
+		while (stream1.peek() == '.' || stream1.peek() == ' ' || stream1.peek() == ',' || stream1.peek() == '<' || stream1.peek() == '>')
+			stream1.ignore();
+
+		std::cout << "num: " << intNum << std::endl;
+	}
+
+}
